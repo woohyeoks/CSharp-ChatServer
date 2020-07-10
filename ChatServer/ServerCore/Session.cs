1e85e2815819be054682457349cad0e2b225b94c
정우혁 <woohyeoks807@naver.com>
2020-07-10 21:06:43 +0900

    [Session] - 세션 추가 및 비동기 recv 작업

---
+++ ChatServer/ServerCore/Session.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    public class Session
+    {
+        Socket m_socket;
+
+        public void Start(Socket _socket)
+        {
+            m_socket = _socket;
+
+            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
+            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+
+            RegisterRecv(recvArgs);
+        }
+
+        #region 네트워크 통신
+
+        private void RegisterRecv(SocketAsyncEventArgs args)
+        {
+            bool pending = m_socket.ReceiveAsync(args);
+            if (pending == false)
+            {
+                OnRecvCompleted(null, args);
+            }
+        }
+
+        private void OnRecvCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+            {
+                try
+                {
+                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
+                    Console.WriteLine($"[From Client] {recvData}");
+                    RegisterRecv(args);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"OnRecvCompleted Failed {e}");
+                }
+            }
+            else
+            {
+
+            }
+        }
+        #endregion
+    }
+}
